bb128cf7d4602b68e31168701eb4bef0e0451352
shiv prasad <163091692+Shivprasadsh@users.noreply.github.com>
2024-11-19 19:42:08 +0530

    new url

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cors({
-  origin:"https://parking-vehicle-ujes.vercel.app/",
+  origin:"https://parking-vehicle-zbdl.vercel.app/?vercelToolbarCode=KYWhcVLbmr7nfsQ/",
   credentials:true
 }));
 app.use(bodyParse.json());
